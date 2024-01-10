def4ac0d048014da68262fa4d402364d0fbef045
Afaque Anwar Azad <afaqueanwaraaa@gmail.com>
2024-01-10 15:25:20 +0530

    initial commits

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 
-	//corev2 "github.com/sensu/core/v2"
+	corev2 "github.com/sensu/core/v2"
 
-	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	// corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-plugin-sdk/sensu"
 )
 
